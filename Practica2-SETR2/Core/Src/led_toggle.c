bdc26abea7bbd3211a3f99e7fc5094553c3efc61
durangoow <durangoow@gmail.com>
2023-10-09 11:04:14 +0200

    Correccion clase leds para garantizar apagado correcto

--- Practica2-SETR2/Core/Src/led_toggle.c
+++ Practica2-SETR2/Core/Src/led_toggle.c
@@ -17,12 +17,18 @@ void led_toggle(uint8_t led) {
 			HAL_GPIO_TogglePin(GPIOA, LED_3_Pin);
 		}	break;
 		case 1:
+			HAL_GPIO_WritePin(GPIOC, LED3_WIFI__LED4_BLE_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOA, LED_3_Pin, GPIO_PIN_RESET);
 			HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
 			break;
 		case 2:
+			HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOA, LED_3_Pin, GPIO_PIN_RESET);
 			HAL_GPIO_TogglePin(GPIOC, LED3_WIFI__LED4_BLE_Pin);
 			break;
 		case 3:
+			HAL_GPIO_WritePin(LED2_GPIO_Port, LED2_Pin, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOC, LED3_WIFI__LED4_BLE_Pin, GPIO_PIN_RESET);
 			HAL_GPIO_TogglePin(GPIOA, LED_3_Pin);
 		default:
 			break;
