615d5e807879fb7257901adc299c2c0d520dadbc
durangoow <durangoow@gmail.com>
2023-10-28 13:36:15 +0200

    Añadida funcion obtiene humedad

--- practica3-SETR2/Core/Src/main.c
+++ practica3-SETR2/Core/Src/main.c
@@ -140,6 +140,7 @@ int main(void)
 	  */
 
 	  Pressure_Test();
+	  Humidity_Test();
 	  HAL_Delay(5000);
     /* USER CODE END WHILE */
 
--- practica3-SETR2/Core/Src/sensores.c
+++ practica3-SETR2/Core/Src/sensores.c
@@ -3,6 +3,7 @@
 
 void init_sensors(void){
 	BSP_PSENSOR_Init();
+	BSP_HSENSOR_Init();
 }
 
 
@@ -11,4 +12,11 @@ void Pressure_Test(void){
 	float press_value=0.;
 	press_value= BSP_PSENSOR_ReadPressure();
 	printf("Presion: %.2f mBar\r\n", press_value);
+}
+
+
+void Humidity_Test(void){
+	float humidity_value=0.;
+	humidity_value= BSP_HSENSOR_ReadHumidity();
+	printf("Humedad: %.2f %%\r\n", humidity_value);
 }
